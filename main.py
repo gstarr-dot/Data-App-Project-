c626d0bc779d0e758738eb64a81e29dc88717fde
gstarr-dot <gstarr@caldwell.edu>
2026-04-29 14:48:18 -0400

    Implement data loading and saving for spending tracker
    Added functions to load and save spending data from a JSON file. Implemented user interface elements for adding and displaying spending.

--- main.py
+++ main.py
@@ -5,6 +5,48 @@ import pandas as pd
 
 File = "spending.json"
 
-def load_data(): 
+def load_data():  #loads saved spending data 
+    if os.path.exists(File):
+        with open(File, "r") as f:
+            return json.load(f)
+    return []
+
+def save_data(data):   #saves spending data
+    with open(File, "w") as f:
+        json.dump(data, f)
+
+def save_data(data):   #saves spending data
+    with open(File, "w") as f:
+        json.dump(data, f)
+
+st.title("Spending Tracker") # Tittle for page
+
+st.header("Add New Spending") # Subheader  
+
+amount = st.number_input("Amount", min_value=0.01, step=0.01)
+category = st.selectbox("Category", ["Food", "Transportation", "Entertainment/Subscriptions", "Other"])
+date = st.date_input("Date")
+
+if st.button("Save spending"):
+    data =load_data() 
+
+    new_expense = {
+        "amount": amount,
+        "category": category,
+        "date": str(date)
+    }
+
+    data.append(new_expense)
+    save_data(data)
+
+#Show all saved and new spending 
+st.header("All Spending")
+
+data = load_data()
+if data:
+    df = pd.DataFrame(data)
+    st.dataframe(df)
+else:
+    st.write("No spending data available.")
 
 
