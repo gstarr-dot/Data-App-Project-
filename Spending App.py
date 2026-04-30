881c11d7468b63244ef69144150168ceed1fce1a
gstarr-dot <gstarr@caldwell.edu>
2026-04-30 16:08:29 -0400

    Refactor Spending App layout and indentation

--- Spending App.py
+++ Spending App.py
@@ -25,35 +25,35 @@ page = st.sidebar.selectbox("Navigation", ["Home", "Charts])
 #home page 
 if page == "Home":
 
-st.title("Spending Tracker") # Tittle for page
+    st.title("Spending Tracker") # Tittle for page
 
-st.header("Add New Spending") # Subheader  
+    st.header("Add New Spending") # Subheader  
 
-#User enters amount, data, and selects category 
-amount = st.number_input("Amount", min_value=0.01, step=0.01)
-category = st.selectbox("Category", ["Food", "Transportation", "Entertainment/Subscriptions", "Other"])
-date = st.date_input("Date")
+    #User enters amount, data, and selects category 
+    amount = st.number_input("Amount", min_value=0.01, step=0.01)
+    category = st.selectbox("Category", ["Food", "Transportation", "Entertainment/Subscriptions", "Other"])
+    date = st.date_input("Date")
 
-if st.button("Save spending"):
-    data =load_data() 
+    if st.button("Save spending"):
+        data =load_data() 
 
-    new_expense = {
+        new_expense = {
         "amount": amount,
         "category": category,
         "date": str(date)
-    }
+        }
 
-    data.append(new_expense)
-    save_data(data)
+        data.append(new_expense)
+        save_data(data)
 
-#Show all saved and new spending 
-st.header("All Spending")
+    #Show all saved and new spending 
+    st.header("All Spending")
 
-data = load_data()
-if data:
-    df = pd.DataFrame(data)
-    st.dataframe(df)
-else:
-    st.write("No spending data available.")
+    data = load_data()
+    if data:
+        df = pd.DataFrame(data)
+        st.dataframe(df)
+    else:
+        st.write("No spending data available.")
 
 
