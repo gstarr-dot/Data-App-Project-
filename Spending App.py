d311d23ef72b762ea91d5e67328b3e4b1bff7f92
gstarr-dot <gstarr@caldwell.edu>
2026-05-10 14:54:35 -0400

    Update Spending App.py

--- Spending App.py
+++ Spending App.py
@@ -89,7 +89,7 @@ if page == "Home":
 
         #display data with filter
         filtered_df["amount"] = filtered_df["amount"].apply(
-            lambda x: f"$(x:.2f)"             #takes value with 2 decimal places
+            lambda x: f"${x:.2f}"             #takes value with 2 decimal places
         )
         st.dataframe(filtered_df)
 
